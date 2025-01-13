1a08d85a1efaaa09e55dd7fba96ec77dbe0b9c72
Lucas Calefi <91427077+LucasCalefi@users.noreply.github.com>
2025-01-13 17:19:10 -0300

    File about Variables
    Notes on classroom subjects, variables and comments.

---
+++ Variavel.java
@@ -0,0 +1,24 @@
+package secao2;
+
+public class Variavel {
+    public static void main(String[] args) {
+    
+        // 1 - O que são variáveis
+        // tipo -> nome -> atribuir um valor;
+        String nome = "Lucas";
+        // nome -> "Lucas"
+        System.out.println(nome);
+
+        // 1 - Atribuição de variável com outra
+        String teste = "Teste";
+
+        String testando = teste;
+
+        System.out.println(testando);
+
+        long numeroGrande = 99999999999999L;
+
+        System.out.println(numeroGrande);
+
+    }
+}
